6d6e12d6976848db7cd6a1382ecd806dec3124ec
dbelliss <daveybelliss@yahoo.com>
2016-07-23 12:33:47 -0700

    first project

---
+++ AnchorAlarm/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  AnchorAlarm
+//
+//  Created by Davey Jay Belliss on 7/23/16.
+//  Copyright © 2016 Davey Jay Belliss. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
